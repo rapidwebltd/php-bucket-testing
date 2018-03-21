43d7170316cc1b2abe825b399a5bce69a7d2bb6a
Jordan Hall <jordan.hall@rapidweb.biz>
2018-03-21 09:25:03 +0000

    Exceptions unit tests

---
+++ tests/Unit/ExceptionsTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use RapidWeb\BucketTesting\BucketManager;
+use RapidWeb\BucketTesting\WeightedBucket;
+use RapidWeb\BucketTesting\Bucket;
+
+final class ExceptionsTest extends TestCase
+{
+
+    public function testGetRandomBucketWhenNoBucketsAreAdded()
+    {
+        $this->expectException(Exception::class);
+        (new BucketManager)->getRandomBucket();
+    }
+
+    public function testCreatingWeightedBucketWithStringWeight()
+    {
+        $this->expectException(Exception::class);
+        (new WeightedBucket(new Bucket('https://php.net/')))->setWeight('not a number!');
+    }
+
+    public function testCreatingWeightedBucketWithFloatWeight()
+    {
+        $this->expectException(Exception::class);
+        (new WeightedBucket(new Bucket('https://php.net/')))->setWeight(2.75);
+    }
+
+    public function testCreatingWeightedBucketWithNegativeWeight()
+    {
+        $this->expectException(Exception::class);
+        (new WeightedBucket(new Bucket('https://php.net/')))->setWeight(-1);
+    }
+
+    public function testCreatingWeightedBucketWithZeroWeight()
+    {
+        $this->expectException(Exception::class);
+        (new WeightedBucket(new Bucket('https://php.net/')))->setWeight(0);
+    }
+
+}
